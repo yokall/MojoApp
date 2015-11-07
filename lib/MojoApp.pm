cf5905dfa15bb99befcff2f018ae02be1df9a16a
Colin Campbell <colincampbell321123@hotmail.com>
2015-11-07 15:02:48 +0000

    Move login logic into controller

--- lib/MojoApp.pm
+++ lib/MojoApp.pm
@@ -4,38 +4,18 @@ use Mojo::Base 'Mojolicious';
 use MojoApp::Model::Users;
 
 sub startup {
-	my $self = shift;
+  my $self = shift;
 
-	$self->secrets(['Mojolicious rocks']);
-	$self->helper(users => sub { state $users = MojoApp::Model::Users->new });
+  $self->secrets(['Mojolicious rocks']);
+  $self->helper(users => sub { state $users = MojoApp::Model::Users->new });
 
-	my $r = $self->routes;
+  my $r = $self->routes;
+  $r->any('/')->to('login#index')->name('index');
 
-	$r->any('/' => sub {
-		my $c = shift;
+  my $logged_in = $r->under('/')->to('login#logged_in');
+  $logged_in->get('/protected')->to('login#protected');
 
-		my $user = $c->param('user') || '';
-		my $pass = $c->param('pass') || '';
-		return $c->render unless $c->users->check($user, $pass);
-
-		$c->session(user => $user);
-		$c->flash(message => 'Thanks for logging in.');
-		$c->redirect_to('protected');
-	} => 'index');
-
-	my $logged_in = $r->under(sub {
-		my $c = shift;
-		return 1 if $c->session('user');
-		$c->redirect_to('index');
-		return undef;
-	});
-	$logged_in->get('/protected');
-
-	$r->get('/logout' => sub {
-		my $c = shift;
-		$c->session(expires => 1);
-		$c->redirect_to('index');
-	});
+  $r->get('/logout')->to('login#logout');
 }
 
 1;
